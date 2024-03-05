ff4bb8850a66d46f8e7d29fb08dd1c5678c2012b
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 03:59:35 +0700

    feat(util) : create helper function

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
-use std::path::Path;
+use std::{fs::create_dir_all, path::Path};
+
+use eyre::Report;
 
 pub fn check_file(src: &Path) -> Option<&Path> {
     if src.is_file() {
@@ -11,4 +13,25 @@ pub fn check_file(src: &Path) -> Option<&Path> {
     } else {
         None
     }
+}
+
+pub fn create_folder(path: &Path) {
+    if !path.exists() {
+        create_dir_all(path).expect("Failed to create output folder")
+    }
+}
+
+pub fn join_path(out_folder: &Path, file_name: &str) -> std::path::PathBuf {
+    out_folder.join(file_name)
+}
+
+pub fn print_err(res: Result<(), Report>) {
+    match res {
+        Ok(_) => (),
+        Err(err) => eprintln!("{}", err),
+    }
+}
+
+pub fn format_name_logo(num: u32) -> String {
+    format!("Square{num}x{num}Logo.png")
 }
