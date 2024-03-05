81ef016792e7a23cf18d0e8fb3b2f603afac60e5
sumoduduk <sumoduduk13@gmail.com>
2024-03-05 23:30:23 +0700

    feat: add path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::path::Path;
+
 use tauri_ico_gen::img_operate::batch_rescale;
 
 fn main() {
-    let _ = batch_rescale();
+    let path = Path::new("./test/test.png");
+    let _ = batch_rescale(path);
 }
