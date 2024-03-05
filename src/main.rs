e8f13eb7495eeaef6dd97da55246403b187ca12a
sumoduduk <sumoduduk13@gmail.com>
2024-03-06 04:01:04 +0700

    feat(main) : add timer and output folder name
    TODO: use clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 use std::path::Path;
+use std::time::Instant;
 
 use tauri_ico_gen::img_operate::batch_rescale;
 
 fn main() {
+    let start = Instant::now();
     let path = Path::new("./test/test.png");
-    let _ = batch_rescale(path);
+    let _ = batch_rescale(path, "./output");
+
+    let duration = start.elapsed();
+    println!("Time duration : {:#?}", duration);
 }
